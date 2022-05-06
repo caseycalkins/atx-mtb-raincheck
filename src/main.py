5034448d5f6ea80dad0075245a2b2fb148c574e8
Casey Calkins <a.caseycalkins@outlook.com>
2022-05-05 21:21:36 -0500

    Created tweet test

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#import TwitterAuth
+from authenticate.auth import TwitterAuth
+
+# Create twitter auth object
+twitter_auth = TwitterAuth()
+# Get the api object
+api = twitter_auth.get_api()
+# Update status
+api.update_status("Hi Mas")
+# Print status
