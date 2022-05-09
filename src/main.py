1ded70e8405ac5e4a88a40e6017f866ef3fe19db
Casey Calkins <a.caseycalkins@outlook.com>
2022-05-08 19:18:36 -0500

    Added test data. Dialed in on the new api

--- src/main.py
+++ src/main.py
@@ -12,14 +12,8 @@ from authenticate.twitter_auth import TwitterAuth
 load_dotenv()
 
 WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
-PATH = "data/trails.json"
+PATH = "data/test_data.json"
 
-# Create twitter auth object
-twitter_auth = TwitterAuth()
-# Get the api object
-api = twitter_auth.get_api()
-# Update status
-# api.update_status("Hi")
 
 def get_yesterdays_date() -> datetime:   
     last_midnight = datetime.combine(datetime.now(), time.min)
@@ -39,18 +33,38 @@ def fetch_weather_data() -> dict:
     trail_weather_data = {}
     trail_data = parse_trail_data_file()
     yesterdays_date = get_yesterdays_date()
+    history_url = "https://api.weatherapi.com/v1/history.json?key={}&q={},{}&dt={}"
+    current_url = "https://api.weatherapi.com/v1/current.json?key={}&q={},{}&aqi=no"
 
+    prep_for_tweet = {}
     for trail, coords in trail_data.items():
-        url = f"https://api.weatherapi.com/v1/history.json?key={WEATHER_API_KEY}&q={coords['lat']},{coords['long']}&dt={yesterdays_date}"
-        r = requests.get(url)
-        if r.status_code == 200:
-            trail_weather_data[trail] = r.json()["forecast"]["forecastday"][0]["day"]["totalprecip_in"]
-            
-    pprint.pprint(trail_weather_data)
-       
+        try:
+            current_weather_request = requests.get(current_url.format(WEATHER_API_KEY, coords['lat'], coords['long'])).json()
+            current_rain_amount = current_weather_request["current"]["precip_in"]
+            history_weather_request = requests.get(history_url.format(WEATHER_API_KEY, coords['lat'], coords['long'], yesterdays_date)).json()
+            historical_rain_amount = history_weather_request["forecast"]["forecastday"][0]["day"]["totalprecip_in"]
 
+            if current_weather_request["current"]["precip_in"] > 0:
+                print(f"{trail} may be experiencing rain. {current_rain_amount} inches recently detected.")
+            elif history_weather_request["forecast"]["forecastday"][0]["day"]["totalprecip_in"] > 0:
+                print(f"{trail} may be wet. {historical_rain_amount} inches of rain within the last 24hrs.")
+        except Exception as e:
+            print(e)
+
+            
+   
 def format_tweet_with_weather_data():
     with open(PATH, "r") as trail_data:
         data = json.load(trail_data)
         return
+
+fetch_weather_data()
+
+# Create twitter auth object
+twitter_auth = TwitterAuth()
+# Get the api object
+api = twitter_auth.get_api()
+# Update status
+# api.update_status("Hi")
+
 
